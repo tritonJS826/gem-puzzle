e1eaf2b66f92af7b34a43eb2ee07de332f266816
triton <triton.68@mail.ru>
2020-04-07 21:48:46 +0300

    fix: counter

--- src/gem-field/tryToShift.js
+++ src/gem-field/tryToShift.js
@@ -12,9 +12,9 @@ function tryToShift(i, j, gemPuzzle) {
   if (gemPuzzle.field[i - 1] && gemPuzzle.field[i - 1][j] === 0) {
     gemPuzzle.swapWithZeroGem(i, j, i - 1, j);
     gemPuzzle.renderField();
-    if (gemPuzzle.isWin()) {
-      alert(`Вы выйграли! Ходов:${gemPuzzle.moveCounter}, Время:${gemPuzzle.date}`);
-    }
+    // if (gemPuzzle.isWin()) {
+    //   alert(`Вы выйграли! Ходов:${gemPuzzle.moveCounter}, Время:${gemPuzzle.date}`);
+    // }
     gemPuzzle.counterPlusOne();
     gemPuzzle.moveCounterRenderer();
     return;
@@ -22,9 +22,9 @@ function tryToShift(i, j, gemPuzzle) {
   if (gemPuzzle.field[i][j - 1] === 0) {
     gemPuzzle.swapWithZeroGem(i, j, i, j - 1);
     gemPuzzle.renderField();
-    if (gemPuzzle.isWin()) {
-      alert(`Вы выйграли! Ходов:${gemPuzzle.moveCounter}, Время:${gemPuzzle.date}`);
-    }
+    // if (gemPuzzle.isWin()) {
+    //   alert(`Вы выйграли! Ходов:${gemPuzzle.moveCounter}, Время:${gemPuzzle.date}`);
+    // }
     gemPuzzle.counterPlusOne();
     gemPuzzle.moveCounterRenderer();
     return;
@@ -32,9 +32,9 @@ function tryToShift(i, j, gemPuzzle) {
   if (gemPuzzle.field[i][j + 1] === 0) {
     gemPuzzle.swapWithZeroGem(i, j, i, j + 1);
     gemPuzzle.renderField();
-    if (gemPuzzle.isWin()) {
-      alert(`Вы выйграли! Ходов:${gemPuzzle.moveCounter}, Время:${gemPuzzle.date}`);
-    }
+    // if (gemPuzzle.isWin()) {
+    //   alert(`Вы выйграли! Ходов:${gemPuzzle.moveCounter}, Время:${gemPuzzle.date}`);
+    // }
     gemPuzzle.counterPlusOne();
     gemPuzzle.moveCounterRenderer();
   }
